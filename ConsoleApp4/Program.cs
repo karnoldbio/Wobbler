5afd4140877baaa249b3bcd3c57f4870be6d9ec9
Kevin <canadiankevin47@gmail.com>
2021-08-28 15:41:52 -0700

    remove docker

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+
+namespace Project2KevinArnoldWobble
+{
+
+    public class Program
+    {
+
+        public Dictionary<string, string[]> Buildpossible(string[] lines)
+        {
+
+
+            Dictionary<string, string[]> rad = new Dictionary<string, string[]>();
+            if (lines.Length == 0)
+            {
+                System.Console.WriteLine("Sequence file is empty");
+                return rad;
+            }
+            Dictionary<char, string> wobbledict = new Dictionary<char, string>()
+            {
+                ['A'] = "WR",
+                ['T'] = "YW",
+                ['C'] = "YS",
+                ['G'] = "RS"
+            };
+            string seqnum = "";
+            foreach (string line in lines)
+            {
+                if (!line.Contains("sequence", StringComparison.OrdinalIgnoreCase))
+                {
+
+                    rad[line] = (from a in wobbledict[line[line.Length - 2]]
+                                 from b in wobbledict[line[line.Length - 1]]
+                                 select line.Remove(line.Length - 2) + a + b).ToArray();
+                }
+                else
+                {
+                    seqnum = line;
+                }
+            }
+            return rad;
+
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+            if (args.Length < 2)
+            {
+                return;
+            }
+            var path = args[0];
+            var res = args[1];
+
+            string[] lines = File.ReadAllLines(path);
+            Program prog = new Program();
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            File.WriteAllText("sequences.json", JsonSerializer.Serialize(prog.Buildpossible(lines), options));
+        }
+
+
+    }
+}
---
+++ ConsoleApp4Tests/ProgramTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+
+namespace Project2KevinArnoldWobble.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void BuildpossibleTestN()
+        {
+            var tprog = new Program();
+            string[] lines = new string[] { ">sequence 1 ", "ATCN" };
+
+            Assert.ThrowsException<KeyNotFoundException>(() => tprog.Buildpossible(lines));
+
+        }
+
+        [TestMethod()]
+        public void BuildpossibleTestEmpty()
+        {
+            var tprog = new Program();
+            string[] lines = new string[] { };
+            var dict = tprog.Buildpossible(lines);
+            var checkdict = new Dictionary<string, string[]>() { };
+            Assert.AreEqual(dict.Keys.Count, checkdict.Keys.Count);
+        }
+    }
+}
